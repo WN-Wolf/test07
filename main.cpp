336638cf6dbc00e85afdf6b8839a6273d47dba89
WN-Wolf <728034416@qq.com>
2023-05-17 23:05:25 +0800

    B的第一次修改

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "A 第一次修改" << endl;
-    // cout << "B 第一次修改" << endl;
+    cout << "B 第一次修改" << endl;
 
     return 0;
 }
--- src/add.cpp
+++ src/add.cpp
@@ -1,6 +1,4 @@
 int add(int a, int b)
 {
-    // A 第一次修改的地方
-    int c = a + b;
-    return c;
+    return a + b; // B的第一次修改的地方
 }
